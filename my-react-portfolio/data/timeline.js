d0d6c0d6d1fa7099e7bd57f8886653b7f8fdb68e
nicholsz <nichols.zane16@gmail.com>
2023-02-23 16:41:19 -0600

    created data to display my timeline

---
+++ my-react-portfolio/data/timeline.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        year: 'Today',
+        title: 'MERN Stack Bootcamp Graduate',
+        duration: '6 Months',
+        details: 'March 9th, 2023 was my graduation date from the Bootcamp, and I will begin the next journey of integrating myself into the working force of web developers. console.log("HELLO WORLD!")',
+    },
+    {
+        year: '2022',
+        title: 'The Climb',
+        duration: '1.5 Years',
+        details: 'With my amazing parents supporting my fiance and I, we were both able to start working towards our passions. My fiance began the long and grueling process of opening a bakery store front. And I began diving into the web developer world by taking a MERN Stack 6 Month Bootcamp at Kansas University.'
+    },
+    {
+        year: '2021',
+        title: 'Difficult Decisions',
+        duration: '2 Years',
+        details: 'With the market crashing and making the budget even tighter, my fiance and I had to make a quick and distruptive decision. We decided to move in with my parents to save money and be able to afford living.'
+    },
+    {
+        year: '2019',
+        title: 'Informatics Degree - COVID-19 College Drop Out',
+        duration: '3.5 Years',
+        details: 'After almost 4 years of college, I felt that I was not learning the skills that I had desired to pursue the career I wanted. So I took a break and joined the fulltime work force. Dedicating my work ethic into climbing my way up the retail corporate ladder.'
+    },
+    {
+        year: '2016',
+        title: 'High School Graduate',
+        duration: '4 Years',
+        details: 'My humble coding beginnings start all the way back to senior year of highschool, when I took a computer applications class for fun. the class mostly covered Microsoft Office applications and their uses. But, thankfully, it had one segment dedicated to a codeacademy.com course.'
+    },
+]
---
+++ my-react-portfolio/src/components/TimelineItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TimelineItem() {
+    return (
+        <div></div>
+    )
+}
+
+export default TimelineItem;
