4004f77c12666927587a520922c45691da651f7c
nicholsz <nichols.zane16@gmail.com>
2023-02-23 14:52:00 -0600

    working on portfolio component

---
+++ my-react-portfolio/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+    title: 'Code-Net',
+    imgUrl: '',
+    stack: ['HTML', 'CSS', 'JavaScript', 'Handlebars'],
+    link: 'https://protected-wave-25502.herokuapp.com/signup/user'
+    },
+    {
+    title: 'Movie-Buffs',
+    imgUrl: '',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    link: 'https://romx10.github.io/Movie-Buffs/'
+    },
+    {
+    title: 'Developer Portfolio',
+    imgUrl: '',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    link: 'deployed site'
+    },
+]
