125cb60dc4ec0ea7051cd36a52e8595b55c69da8
nicholsz <nichols.zane16@gmail.com>
2023-02-23 21:35:00 -0600

    finished

--- data/portfolio.js
+++ data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
     title: 'Code-Net',
-    imgUrl: '../public/assets/code-net.jpg',
+    imgUrl: 'assets/code-net.jpg',
     stack: [ 'HTML', 'CSS', 'JavaScript', 'Handlebars' ],
     link: 'https://protected-wave-25502.herokuapp.com/signup/user'
     },
     {
     title: 'Movie-Buffs',
-    imgUrl: '../public/assets/movie-buffs.png',
+    imgUrl: 'assets/movie-buffs.png',
     stack: [ 'HTML', 'CSS', 'JavaScript' ],
     link: 'https://romx10.github.io/Movie-Buffs/'
     },
     {
     title: 'Developer Portfolio',
-    imgUrl: '../public/assets/portfolio.png',
+    imgUrl: 'assets/portfolio.png',
     stack: [ 'React', 'Tailwind', 'Vite' ],
     link: 'https://nicholszane13.github.io/React-Portfolio/'
     },
